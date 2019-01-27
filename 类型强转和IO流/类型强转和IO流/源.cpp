10c2786e3353ee765eaeee7cca92168778ce1a85
xiaoweixiao <1679204951@qq.com>
2019-01-27 11:08:28 +0800

    完成C++强制类型转换博客

--- 类型强转和IO流/类型强转和IO流/源.cpp
+++ 类型强转和IO流/类型强转和IO流/源.cpp
@@ -35,14 +35,66 @@
 //	return 0;
 //}
 
+
+//#include<iostream>
+//using namespace std;
+//
+//class Person{
+//protected:
+//	string _name;
+//	virtual void Print()
+//	{
+//		cout << "I am the father" << endl;
+//	}
+//};
+//
+//class Student :public Person
+//{
+//public:
+//	int _num;
+//	virtual void Print()
+//	{
+//		cout << "I am the son" << endl;
+//	}
+//};
+//void Func(Person* p)
+//{
+//	Student* sp = dynamic_cast<Student*>(p);//只允许子类对象指针转换
+//	sp->Print();
+//}
+//int main()
+//{
+//	Person p;
+//	Student s;
+//	//Func(&p);//使用dynamic_cast，不能将父类指针转换成子类指针，防止了越界行为
+//	Func(&s);
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a = 10;
+//	int*p = static_cast<int*>(&a);
+//	*p = 20;
+//	cout << a << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
 #include<iostream>
+#include<string>
 using namespace std;
 int main()
 {
 	int a = 10;
-	int*p = static_cast<int*>(&a);
-	*p = 20;
-	cout << a << endl;
+	int*p = &a;
+
+	int b = reinterpret_cast<int>(p);
 	system("pause");
 	return 0;
 }
