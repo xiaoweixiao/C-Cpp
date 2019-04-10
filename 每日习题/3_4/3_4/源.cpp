ff291a4cc4c55dd8928594226d3680665f7317aa
xiaoweixiao <1679204951@qq.com>
2019-04-10 15:15:19 +0800

    clear up agin

--- 每日习题/3_4/3_4/源.cpp
+++ 每日习题/3_4/3_4/源.cpp
@@ -1,3 +1,27 @@
+
+
+//选择1
+//int main()
+//{
+//	int x, y, count = 0;
+//	for (x = 0, y = 0; (y = 123) && (x < 4); x++)
+//		count++;
+//	cout << count << endl;
+//	system("pause");
+//}
+
+//选择2
+//#include<iostream>
+//using namespace std;
+//
+//int main(void)
+//{
+//	printf("%s , %5.3s\n", "computer", "computer");
+//	system("pause");
+//	return 0;
+//}
+
+
 //编程1题
 //#include<iostream>
 //#include<vector>
@@ -8,21 +32,54 @@
 //	int n;
 //	while (cin >> n)
 //	{
-//		vector<int> level(3 * n);
-//		for (int i = 0; i < (3 * n); i++)
-//			cin >> level[i];
+//		long long ret = 0;//使用int，通过率66％
+//		vector<int> in(3 * n);
+//		for (int i = 0; i<3 * n; i++)
+//			cin >> in[i];
+//		sort(in.begin(), in.end());
 //
-//		sort(level.begin(), level.end());
-//		
-//		long long sum = 0;
-//		for (int i = n; i <= 3 * n - 2; i += 2)
-//			sum += level[i];
-//		cout << sum << endl;
+//		//1    2    5    5    5    8
+//		//3*n-2是倒数第二个数，也就是最后一组的中位数
+//		//第一组的中位数下标是n，0至n-1是每组最小数
+//		for (int i = n; i <= 3 * n - 2; i += 2)//i+=2，中间空出来的是上一组最大的数
+//			ret += in[i];
+//		cout << ret << endl;
 //	}
 //	return 0;
 //}
 
 
+//编程2题
+
+//O(s1.size()*s2.size())
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s1;
+//	string s2;
+//	string ret;
+//	while (getline(cin, s1))
+//	{
+//		getline(cin, s2);
+//		for (size_t j = 0; j<s1.size(); j++)
+//		{
+//			size_t i = 0;
+//			for (; i<s2.size(); i++)
+//			{
+//				if (s1[j] == s2[i])
+//					break;
+//			}
+//			if (i == s2.size())
+//				ret.push_back(s1[j]);
+//		}
+//		cout << ret << endl;
+//	}
+//	return 0;
+//}
+
+//O(s2.size()+256)
 #include<iostream>
 #include<string>
 using namespace std;
@@ -36,9 +93,7 @@ int main()
 		getline(cin, str2);
 		int hash[256] = { 0 };
 		for (int i = 0; i < str2.size(); i++)
-		{
 			hash[str2[i]]++;
-		}
 
 		for (int i = 0; i < str1.size(); i++)
 		{
@@ -48,27 +103,3 @@ int main()
 		cout << ret << endl;
 	}
 }
-
-
-
-//1
-//int main()
-//{
-//	int x, y, count = 0;
-//	for (x = 0, y = 0; (y = 123) && (x < 4); x++)
-//		count++;
-//	cout << count << endl;
-//	system("pause");
-//}
-
-//2
-//#include<iostream>
-//using namespace std;
-//
-//int main(void)
-//{
-//	printf("%s , %5.3s\n", "computer", "computer");
-//	system("pause");
-//	return 0;
-//}
-
--- 每日习题/3_5/3月5日/源.cpp
+++ 每日习题/3_5/3月5日/源.cpp
@@ -78,31 +78,36 @@
 //}
 
 //编程1题
-#include<iostream>
-#include<vector>
-using namespace std;
-int main()
-{
-	int n;
-	while (cin>>n)
-	{
-		vector<int> num(n);
-		int ret = 1;
-		for (int i = 0; i < n; i++)
-			cin >> num[i];
-		for (int i = 1; i < n-1; i++)
-		{
-			if ((num[i]>num[i - 1] && num[i]>num[i + 1]) || (num[i] < num[i - 1] && num[i] < num[i + 1]))
-			{
-				ret++;
-				if (i != n - 3)
-					i++;
-			}
-			
-		}
-		cout << ret << endl;
-	}
-}
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	while (cin >> n)
+//	{
+//		vector<int> in(n);
+//		for (int i = 0; i<n; i++)
+//			cin >> in[i];
+//
+//		int team = 1;
+//		//找波峰或者波谷
+//		for (int i = 1; i<n - 1; i++)
+//		{
+//			if ((in[i]>in[i - 1] && in[i]>in[i + 1]) || (in[i] < in[i - 1] && in[i] < in[i + 1]))
+//			{
+//				team++;
+//
+//				//波峰的下一个位置已经比较过了必须跳过，比如1 2 1 2 1 2 1 2 1就会出错
+//				if (i != n - 3)
+//					i++;
+//			}
+//			
+//		}
+//		cout << team << endl;
+//	}
+//	return 0;
+//}
 //编程2题
 //#include<iostream>
 //#include<string>
@@ -131,3 +136,19 @@ int main()
 //	}
 //	return 0;
 //}
+
+#include <iostream>
+#include <string>
+using namespace std;
+// I like beijing.
+//beijing.like I
+int main()
+{
+	//类似于将单词一个个头插到str
+	string str, temp;
+	cin >> str;
+	while (cin >> temp)
+		str = temp + " " + str;
+	cout << str << endl;
+	return 0;
+}
